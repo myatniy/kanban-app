d92baa46c8d6ee8af8356d9f9baba14bc669e1f4
Ilya <ipsixin3d@gmail.com>
2020-07-13 02:06:50 +0300

    Updates

---
+++ precache-manifest.8da011c8dc209f62bbc84ee77f336d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a295d47edca7f656c0ee6db386ffa7",
+    "url": "/reactTodoApplication/index.html"
+  },
+  {
+    "revision": "749e54712eda6ac746a0",
+    "url": "/reactTodoApplication/static/css/main.c4a00961.chunk.css"
+  },
+  {
+    "revision": "3582e2855675a88e2bb1",
+    "url": "/reactTodoApplication/static/js/2.177fb567.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTodoApplication/static/js/2.177fb567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749e54712eda6ac746a0",
+    "url": "/reactTodoApplication/static/js/main.ea282694.chunk.js"
+  },
+  {
+    "revision": "8c9df86314735f668177",
+    "url": "/reactTodoApplication/static/js/runtime-main.e678330f.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/reactTodoApplication/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
